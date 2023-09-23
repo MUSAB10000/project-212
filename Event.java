f55cf6d634dea2afcc3f6548bd1b5f6ea0652019
abdulazizAlobud <3ziz1424@gmail.com>
2023-09-23 13:45:38 +0300

    Event

--- Event.java
+++ Event.java
@@ -1,3 +1,29 @@
 public class Event {
+private String EventTitle, DataAndTime, Location;
+private Contact Contact_inv;
 
+public String getEventTitle() {
+		return EventTitle;
+	}
+	public void setEventTitle(String eventTitle) {
+		EventTitle = eventTitle;
+	}
+	public String getDataAndTime() {
+		return DataAndTime;
+	}
+	public void setDataAndTime(String dataAndTime) {
+		DataAndTime = dataAndTime;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	public Contact getContact_inv() {
+		return Contact_inv;
+	}
+	public void setContact_inv(Contact contact_inv) {
+		Contact_inv = contact_inv;
+    }
 }
