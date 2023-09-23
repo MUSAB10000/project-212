8a7adfb5f6110266e7c85d18d654b346eda7771c
Abdullah92o <145840830+Abdullah92o@users.noreply.github.com>
2023-09-23 13:54:20 +0300

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -1,3 +1,46 @@
 public class Contact {
-
+private String ContactName;
+    private int PhoneNumber;
+    private String Email;
+    private String address;
+    private String Birthday;
+    private String notes;
+    public int getPhoneNumber() {
+        return PhoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+    public String getContactName() {
+        return ContactName;
+    }
+    public void setContactName(String contactName) {
+        ContactName = contactName;
+    }
+    public String getEmail() {
+        return Email;
+    }
+    public void setEmail(String email) {
+        Email = email;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+   
+    public String getBirthday() {
+        return Birthday;
+    }
+    public void setBirthday(String birthday) {
+        Birthday = birthday;
+    }
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
