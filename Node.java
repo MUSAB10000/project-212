acba880e08b42296ff0f2fddaea67980231c7173
Abdullah92o <145840830+Abdullah92o@users.noreply.github.com>
2023-10-09 03:49:05 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,6 +1,22 @@
 public class Node<w> {
     public w data;
     public Node<w> next;
+    public w getData() {
+        return data;
+    }
+
+    public void setData(w data) {
+        this.data = data;
+    }
+
+    public Node<w> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<w> next) {
+        this.next = next;
+    }
+
 
     public Node(w data) {
         this.data = data;
