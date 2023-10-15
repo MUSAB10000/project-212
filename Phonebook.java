76bedb14872ceb71373273e6ef6a62efb57e8411
MUSAB10000 <140024767+MUSAB10000@users.noreply.github.com>
2023-10-15 22:15:15 +0300

    aaa

--- Phonebook.java
+++ Phonebook.java
@@ -2,130 +2,146 @@ import java.util.Scanner;
 import java.util.Date;
 
 public class Phonebook {
+
     LinkedList<Contact> contact;
     LinkedList<Event> event;
     public Scanner input = new Scanner(System.in);
 
-    public Phonebook(){
-        contact = new LinkedList<>();;
-        event = new LinkedList<>();;
+    public Phonebook() {
+        contact = new LinkedList<>();
+        ;
+        event = new LinkedList<>();
+        ;
     }
 
     public void display() {
-        System.out.println("Welcome to the Linked Tree Phonebook!\n" +
-                "Please choose an option:\n" +
-                "1. Add a contact\n" +
-                "2. Search for a contact\n" +
-                "3. Delete a contact\n" +
-                "4. Schedule an event\n" +
-                "5. Print event details\n" +
-                "6. Print contacts by first name\n" +
-                "7. Print all events alphabetically\n" +
-                "8. Exit");
-        int choice = input.nextInt();
-        switch (choice) {//start big switch
-            case 1:
-                System.out.print("Enter the contact's name: ");
-                String name = input.nextLine();
+        int choice = 0;
+        System.out.println("Welcome to the Linked Tree Phonebook!\n");
+        do {
+            System.out.println(
+                    "Please choose an option:\n" +
+                            "1. Add a contact\n" +
+                            "2. Search for a contact\n" +
+                            "3. Delete a contact\n" +
+                            "4. Schedule an event\n" +
+                            "5. Print event details\n" +
+                            "6. Print contacts by first name\n" +
+                            "7. Print all events alphabetically\n" +
+                            "8. Exit");
+            choice = input.nextInt();
 
-                System.out.print("Enter the contact's phone number: ");
-                String phoneNumber = input.nextLine();
+            switch (choice) {// start big switch
+                case 1:
+                    System.out.print("Enter the contact's name: ");
+                    String name = input.next();
 
-                System.out.print("Enter the contact's email address: ");
-                String email = input.nextLine();
+                    System.out.print("\nEnter the contact's phone number: ");
+                    String phoneNumber = input.next();
 
-                System.out.print("Enter the contact's address: ");
-                String address = input.nextLine();
+                    System.out.print("\nEnter the contact's email address: ");
+                    String email = input.next();
 
-                System.out.print("Enter the contact's birthday: ");
-                String birthday = input.nextLine();
+                    System.out.print("Enter the contact's address: ");
+                    String address = input.next();
 
-                System.out.print("Enter any notes for the contact: ");
-                String notes = input.nextLine();
-                Contact x = new Contact(name, phoneNumber, email, address, birthday, notes);
-                contact.add(x);
-                break;
-            case 2:
-                System.out.print("Enter search criteria:\n" +
-                        "1. Name\n" +
-                        "2. Phone Number\n" +
-                        "3. Email Address\n" +
-                        "4. Address\n" +
-                        "5. Birthday\n" +
-                        "Enter your choice: ");
-                int choice2 = input.nextInt();
-                switch(choice2) {//start small switch
-                    case 1:System.out.println("Enter the contact's name:");
-                           name = input.next();
-                           contact.SearchContact(contact.getHead(),name, choice2);
-                           break;
-                    case 2:System.out.println("Enter the contact's name:");
-                           phoneNumber = input.next();
-                           contact.SearchContact(contact.getHead(),phoneNumber, choice2);
-                           break;
-                    case 3:System.out.println("Enter the contact's name:");
-                           email = input.next();
-                           contact.SearchContact(contact.getHead(),email, choice2);
-                           break;
-                    case 4:System.out.println("Enter the contact's name:");
-                           address = input.next();
-                           contact.SearchContact(contact.getHead(),address, choice2);
-                           break;
-                    case 5:System.out.println("Enter the contact's name:");
-                           birthday = input.next();
-                           contact.SearchContact(contact.getHead(),birthday, choice2);
-                           break;
-                    default: System.out.println("Wrong number, please do it again");
-                }//end small switch
-                break;
-            case 3:
-                System.out.println("Enter phone number to delete a contact");
-                String phone = input.nextLine();
-                contact.Remove(phone);
-                break;
-            case 4:
-                System.out.print("Enter event title: ");
-                String eventTitle = input.nextLine();
+                    System.out.print("Enter the contact's birthday: ");
+                    String birthday = input.next();
 
-                System.out.print("Enter contact name: ");
-                String contactName = input.nextLine();
+                    System.out.print("Enter any notes for the contact: ");
+                    String notes = input.next();
+                    Contact x = new Contact(name, phoneNumber, email, address, birthday, notes);
+                    contact.add(x);
+                    break;
+                case 2:
+                    System.out.print("Enter search criteria:\n" +
+                            "1. Name\n" +
+                            "2. Phone Number\n" +
+                            "3. Email Address\n" +
+                            "4. Address\n" +
+                            "5. Birthday\n" +
+                            "Enter your choice: ");
+                    int choice2 = input.nextInt();
+                    switch (choice2) {// start small switch
+                        case 1:
+                            System.out.println("Enter the contact's name:");
+                            name = input.next();
+                            contact.SearchContact(contact.getHead(), name, choice2);
+                            break;
+                        case 2:
+                            System.out.println("Enter the contact's name:");
+                            phoneNumber = input.next();
+                            contact.SearchContact(contact.getHead(), phoneNumber, choice2);
+                            break;
+                        case 3:
+                            System.out.println("Enter the contact's name:");
+                            email = input.next();
+                            contact.SearchContact(contact.getHead(), email, choice2);
+                            break;
+                        case 4:
+                            System.out.println("Enter the contact's name:");
+                            address = input.next();
+                            contact.SearchContact(contact.getHead(), address, choice2);
+                            break;
+                        case 5:
+                            System.out.println("Enter the contact's name:");
+                            birthday = input.next();
+                            contact.SearchContact(contact.getHead(), birthday, choice2);
+                            break;
+                        default:
+                            System.out.println("Wrong number, please do it again");
+                    }// end small switch
+                    break;
+                case 3:
+                    System.out.println("Enter phone number to delete a contact");
+                    String phone = input.next();
+                    contact.Remove(phone);
+                    break;
+                case 4:
+                    System.out.print("Enter event title: ");
+                    String eventTitle = input.next();
 
-                System.out.print("Enter event date and time (MM/DD/YYYY HH:MM): ");
-                Date dateTime = new Date(input.nextLine());
+                    System.out.print("Enter contact name: ");
+                    String contactName = input.next();
 
-                System.out.print("Enter event location: ");
-                String location = input.nextLine();
-                Contact contact = new Contact(contactName);
-                Event event1 = new Event(eventTitle, dateTime, location, contact);
-                event.add(event1);
-                break;
-            case 5:
+                    System.out.print("Enter event date and time (MM/DD/YYYY HH:MM): ");
+                    Date dateTime = new Date(input.next());
+
+                    System.out.print("Enter event location: ");
+                    String location = input.next();
+                    Contact contact = new Contact(contactName);
+                    Event event1 = new Event(eventTitle, dateTime, location, contact);
+                    event.add(event1);
+                    break;
+                case 5:
                     System.out.println("Enter search criteria:\r\n" + //
-                    "1. contact name\r\n" + //
-                    "2. Event tittle");
+                            "1. contact name\r\n" + //
+                            "2. Event tittle");
                     choice2 = input.nextInt();
                     switch (choice2) {
                         case 1:
-                        System.out.println("Enter the Contact Name:");
-                        contactName = input.nextLine();
-                        event.PrintEvent(event.getHead(), contactName, choice2);
-                        break;
+                            System.out.println("Enter the Contact Name:");
+                            contactName = input.next();
+                            event.PrintEvent(event.getHead(), contactName, choice2);
+                            break;
                         case 2:
-                        System.out.println("Enter the event title:");
-                        eventTitle = input.nextLine();
-                        event.PrintEvent(event.getHead(), eventTitle, choice2);
-                        break;
+                            System.out.println("Enter the event title:");
+                            eventTitle = input.next();
+                            event.PrintEvent(event.getHead(), eventTitle, choice2);
+                            break;
                         default:
-                        System.out.println("Wrong number, please do it again");
+                            System.out.println("Wrong number, please do it again");
                     }
-                break;
+                    break;
 
-            case 6:
+                case 6:
 
-                break;
-            case 7: event.PrintAllEvent();
-                break;
+                    break;
+                case 7:
+                    event.PrintAllEvent();
+                    break;
 
-        }//end big switch
+            }
+        } while (choice != 8);
+        // end big switch
     }
 }
--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Phonebook p = new Phonebook();
+        p.display();
     }
 }
